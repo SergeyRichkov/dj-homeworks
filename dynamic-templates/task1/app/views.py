f835612042dfc5a82a58b8f466bb6606cba5b202
Sergey Richkov <sergey.rychkov.2016@gmail.com>
2020-10-19 21:46:20 +0700

    for chek2

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.conf import settings
 import csv
 
+
 def inflation_view(request):
     template_name = 'inflation.html'
 
-    inflation_table = []
     inflation_info = []
     title_iflation_row = []
     with open(settings.INFLATION_CSV, newline='', encoding='utf-8') as csvfile:
@@ -13,23 +13,19 @@ def inflation_view(request):
         dict_reader = csv.DictReader(csvfile, delimiter=';')
         for row in reader:
             title_iflation_row.append(row)
-            inflation_table.append(row[1:-1])
         csvfile.seek(0)
         for row2 in dict_reader:
             inflation_info.append({
                 'Year': row2['Год'],
-                'Jan': row2['Янв'], 'Feb': row2['Фев'],'Mar': row2['Мар'],
-                'Apr': row2['Апр'], 'May': row2['Май'],'Jun': row2['Июн'],
-                'Jul': row2['Июл'], 'Aug': row2['Авг'],'Sep': row2['Сен'],
-                'Oct': row2['Окт'], 'Nov': row2['Ноя'],'Dec': row2['Дек'],
+                'Jan': row2['Янв'], 'Feb': row2['Фев'], 'Mar': row2['Мар'],
+                'Apr': row2['Апр'], 'May': row2['Май'], 'Jun': row2['Июн'],
+                'Jul': row2['Июл'], 'Aug': row2['Авг'], 'Sep': row2['Сен'],
+                'Oct': row2['Окт'], 'Nov': row2['Ноя'], 'Dec': row2['Дек'],
                 'Summ': row2['Суммарная']})
 
-
     title_row = title_iflation_row[0]
-    years = inflation_table[1:]
 
     context = {'title_row': title_row,
-               'years': years,
                'inflation_info': inflation_info
                }
 
